6f6b851033bfee998aa8a195a5070b5e2a3c87c0
Beatriz Nascimento <beatriz-vn@hotmail.com>
2025-05-07 10:16:46 -0300

    feat(backend): init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { Pool } from 'pg';
+import cors from 'cors';
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'bd',
+  password: 'postgres',
+  port: 5432,
+});
+
+app.get('/api/hello', async (_, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Backend running at http://localhost:${port}`);
+});
