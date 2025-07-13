44093caf380b94ba0b3f682523112f03e1cb0414
Beatriz Nascimento <73648297+Beatrizvn@users.noreply.github.com>
2025-07-13 16:45:14 -0300

    feat(frontend): create header

---
+++ frontend/components/Header/index.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface HeaderProps {
+  isLoggedIn: boolean;
+}
+
+export default function Header({ isLoggedIn }: HeaderProps) {
+  return (
+    <header
+      className="
+        w-full                     
+        bg-white                   
+        px-6 sm:px-10 py-4          
+        border-b border-gray-200   
+        flex items-center 
+      "
+    >
+      <Link href="/" className="flex items-center gap-3 ml-15">
+        <Image
+          src="/images/logo.png"
+          alt="Sales Control Logo"
+          width={120}
+          height={40}
+        />
+      </Link>
+
+      {isLoggedIn && (
+        <nav className='flex ml-20 text-base font-semibold'>
+          <ul className="flex items-center gap-6 md:gap-8">
+            <li>
+              <Link
+                href="/"
+                className="text-dark hover:text-black transition-colors"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/sales"
+                className="text-dark hover:text-black transition-colors"
+              >
+                Sales
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/products"
+                className="text-dark hover:text-black transition-colors"
+              >
+                Products
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+}
